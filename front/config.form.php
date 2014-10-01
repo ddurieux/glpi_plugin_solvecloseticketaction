c3e75f2121a0589997911518265960f7fab2e9ad
David Durieux <d.durieux@siprossii.com>
2014-10-02 00:45:51 +0200

    Add license

--- front/config.form.php
+++ front/config.form.php
@@ -1,5 +1,45 @@
 <?php
 
+/*
+   ------------------------------------------------------------------------
+   Plugin Solvecloseticketaction for GLPI
+   Copyright (C) 2014-2014 by the Plugin Solvecloseticketaction for David Durieux.
+
+   https://
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of Plugin Solvecloseticketaction project.
+
+   Plugin Solvecloseticketaction for GLPI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Plugin Solvecloseticketaction for GLPI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Solvecloseticketaction. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   Plugin Solvecloseticketaction for GLPI
+   @author    David Durieux
+   @co-author
+   @comment
+   @copyright Copyright (c) 2014-2014 Plugin Solvecloseticketaction for David Durieux
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      https://
+   @since     2014
+
+   ------------------------------------------------------------------------
+ */
+
 define('GLPI_ROOT', '../../..');
 include (GLPI_ROOT . "/inc/includes.php");
 
--- hook.php
+++ hook.php
@@ -1,8 +1,48 @@
 <?php
 
+/*
+   ------------------------------------------------------------------------
+   Plugin Solvecloseticketaction for GLPI
+   Copyright (C) 2014-2014 by the Plugin Solvecloseticketaction for David Durieux.
+
+   https://
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of Plugin Solvecloseticketaction project.
+
+   Plugin Solvecloseticketaction for GLPI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Plugin Solvecloseticketaction for GLPI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Solvecloseticketaction. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   Plugin Solvecloseticketaction for GLPI
+   @author    David Durieux
+   @co-author
+   @comment
+   @copyright Copyright (c) 2014-2014 Plugin Solvecloseticketaction for David Durieux
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      https://
+   @since     2014
+
+   ------------------------------------------------------------------------
+ */
+
 function plugin_solvecloseticketaction_install() {
    global $DB;
-   
+
    $query = "CREATE TABLE IF NOT EXISTS `glpi_plugin_solvecloseticketaction_configs` (
    `id` int(11) NOT NULL AUTO_INCREMENT,
    `entities_id` int(11) NOT NULL DEFAULT '0',
@@ -11,22 +51,22 @@ function plugin_solvecloseticketaction_install() {
    PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
    $DB->query($query);
-   
-   $query = "INSERT INTO `glpi_plugin_solvecloseticketaction_configs` 
-      (`id`, `entities_id`, `createfollowupwithsolve`, `assigntechsolveticket`) 
+
+   $query = "INSERT INTO `glpi_plugin_solvecloseticketaction_configs`
+      (`id`, `entities_id`, `createfollowupwithsolve`, `assigntechsolveticket`)
       VALUES (1, 0, '0', '0');";
    $DB->query($query);
-   
+
    return true;
 }
 
 // Uninstall process for plugin : need to return true if succeeded
 function plugin_solvecloseticketaction_uninstall() {
    global $DB;
-   
+
    $query = "DROP TABLE `glpi_plugin_solvecloseticketaction_configs`";
    $DB->query($query);
-   
+
    return true;
 }
 
@@ -37,7 +77,7 @@ function plugin_get_headings_solvecloseticketaction($item,$withtemplate) {
    global $LANG;
 
    switch (get_class($item)) {
-      
+
       case 'Entity':
          $array = array();
          if ($item->getID() > 0) {
@@ -57,13 +97,13 @@ function plugin_get_headings_solvecloseticketaction($item,$withtemplate) {
 function plugin_headings_actions_solvecloseticketaction($item) {
 
    switch (get_class($item)) {
-      
+
       case 'Entity':
          $array = array();
          $array[0] = "plugin_headings_solvecloseticketaction_config";
          return $array;
          break;
-      
+
    }
    return false;
 }
